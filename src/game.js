2caa05f55a0250baca0e8d4d9b95a693c83f5aed
19L-Games <73015630+19L-Games@users.noreply.github.com>
2020-11-13 17:36:31 +0000

    Update game.js

--- src/game.js
+++ src/game.js
@@ -1,8 +1,22 @@
 var canvas = document.getElementById("canvas");
 var c = canvas.getContext("2d");
-function test() {
-	c.fillRect(0,0,250,250);
+
+var blanklevel = document.getElementById("blanklevel");
+var level1 = document.getElementById("level1");
+var currentimg = blanklevel;
+
+function setup() {
+	window.setInterval(loop,1000/60);
+	document.getElementById("loading").style = "display:none"
+}
+
+function loop() {
+	clear(currentimg);
 	
-	
+}
+
+
+function clear(img) {
+	c.drawImage(img,0,0);
 	
 }

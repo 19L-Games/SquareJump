d60a55a021537048d4c44c6ae6b1b60fd633479f
19L-Games <73015630+19L-Games@users.noreply.github.com>
2021-01-03 20:13:59 +0000

    Update game.js

--- src/game.js
+++ src/game.js
@@ -20,29 +20,33 @@ var down = document.getElementById("down");
 var currentimg = blanklevel;
 
 
+
 function setup() {
+
     //this is called when the game starts
- 
     user = new player(50, 300, playerblue, 0.3333, 4);
-	jsoncode = getRemote("https://www.jsonblob.com/api/bb972a1f-2738-11eb-990f-bf95cb2273ee");
+
+    jsoncode = getRemote("https://www.19lgames.tk/games/squarejump/webdata.json");
     webdata = JSON.parse(jsoncode);
-    if (webdata.version == "v3.0-beta.4") {
-        console.log("You are running the latest version");
+    if (webdata.version == "v3.0-beta.5") {
+        console.log("%c You are running the latest version", "background: #90ee90");
     } else {
         ood();
+        console.warn("You are running an out of date version. Download the latest version from: https://www.github.com/19L-Games/SquareJump/tags");
     }
-     if (webdata.down == false) {
-        console.log("Servers are online");
+    if (webdata.down == false) {
+        console.log('%c Servers are online', 'background: #90ee90');
     } else {
         currentimg = down;
-		user.visible = false;
+        user.visible = false;
+        console.error("Servers are down");
     }
-	
+
     window.addEventListener("keydown", key);
     window.addEventListener("keyup", keyup);
     window.setInterval(loop, 1000 / 60);
     document.getElementById("loading").style = "display:none"
-	document.getElementById("news").innerHTML = webdata.news;
+    document.getElementById("news").innerHTML = webdata.news;
 }
 
 function loop() {
